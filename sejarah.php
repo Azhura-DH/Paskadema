cf86b861f6f7c67b7fec5c62e6ffd5e56f77217e
araraso <firasaddd@gmail.com>
2019-10-10 15:09:45 +0700

    nambah sejarah

--- sejarah.php
+++ sejarah.php
@@ -2,7 +2,36 @@
     <?php
         include 'headers/head.php'
     ?>
-
+    <div class="jumbotron">
+        <h1>Sejarah Paskibra Unit SMK Negeri 8 Malang</h1>
     
+        <img src="./images/logo.jpg" width= "100" height="100">
+        <p>Paskibra Unit SMK Negeri 8 Malang berdiri pada 11 Maret 2008. 
+        Paskibra Unit SMK Negeri 8 Malang  berdiri atas dasar rintisan AD/ART (Aturan Dasar Rumah Tangga) pada tahun 2007 - 2008.
+        <br>Pemrakarsa Paskibra unit SMK 8 ada 18 orang, yaitu:<br>
+        <br> 
+        1.	Agam Gahara <br>
+        2.	Dedy Setyawan<br>
+        3.	Dita Yutika<br>
+        4.	Dwi Febrian<br>
+        5.	Fakhmi Hasqia<br>
+        6.	Hamdan IR<br>
+        7.	Indri<br>
+        8.	M. Taufik<br>
+        9.	Mega Febrianti<br>
+        10.	Meko Gery M<br>
+        11.	Nofia Alfiani<br>
+        12.	Nofian Firmansyah<br>
+        13.	Nur Azizah<br>
+        14.	Robin Janitra<br>
+        15.	Tika Resti<br>
+        16.	Winsy Grace<br>
+        17.	Yogi Medi<br>
+        18.	Laksono Probo<br>
+        <br>
+        Dari pemrakarsa rintisan tersebut disertai dengan ajuan wakasis dan pembina OSIS. 
+        Pengajuan proposal Paskibra diserahkan kepada PPI (Purna Paskibra Indonesia) pada 14 Maret 2008.
+        </p>
+    </div>  
 </body>
 </html>
