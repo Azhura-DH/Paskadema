5eed4add59ec9ac1d312527e3255cde3543e16f7
DillaHiro <dillabraveshine@gmail.com>
2019-10-23 15:24:42 +0700

    changed -> news theme to card list

--- news.php
+++ news.php
@@ -4,28 +4,23 @@
         include 'admin/inc/config.php';
     ?>
 
-    <div class="container mt-5">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-sm-9">
                 <?php
                     $query = mysqli_query($mysqli,"SELECT * FROM news ORDER BY id_news DESC");
                     
                     if(mysqli_num_rows($query) > 0) {
                         while($data = mysqli_fetch_array($query)) {?>
-                            <tr>
-                                <td><?=$data["id_news"]?></td>
-                                <td><?=$data["title_news"]?></td>
-                                <td><?=$data["description"]?></td>
-                                <td><?=$data["date_news"]?></td>
-                            </tr>
+                            <div class="card mb-2">
+                                <div class="date">
+                                    <p><?=$data['date_news']?></p>
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title"><?=$data['title_news']?></h4>
+                                    <p class="card-text"><?=$data['description']?></p>
+                                </div>
+                            </div>
                         <?php
                         }
                     } else {
@@ -36,8 +31,30 @@
                         ";
                     }
                 ?>
-            </tbody>
-        </table>
+            </div>
+            <div class="col-sm-3">
+                <?php
+                    $query = mysqli_query($mysqli,"SELECT * FROM news_category ORDER BY category");
+                    
+                    if(mysqli_num_rows($query) > 0) {
+                        while($data = mysqli_fetch_array($query)) {?>
+                            <div class="card mb-2">
+                                <div class="card-body">
+                                    <p class="card-text"><?=$data['category']?></p>
+                                </div>
+                            </div>
+                        <?php
+                        }
+                    } else {
+                        echo "
+                            <tr>
+                                <td>Tidak ada data.</td>
+                            </tr>
+                        ";
+                    }
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
